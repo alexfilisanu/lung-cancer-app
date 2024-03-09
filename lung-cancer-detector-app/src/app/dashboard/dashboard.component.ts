b9473f49e9e5245264b1da4b45c1efa5c5992799
alexfilisanu <mihai.filisanu@gmail.com>
2024-03-09 23:42:13 +0200

    added dashboard carousel

--- lung-cancer-detector-app/src/app/dashboard/dashboard.component.ts
+++ lung-cancer-detector-app/src/app/dashboard/dashboard.component.ts
@@ -13,5 +13,25 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  public slides: Array<string> = ['Slide 1', 'Slide 2', 'Slide 3'];
+  public currentSlideIndex: number = 0;
+  private slideInterval: number | undefined;
 
+  constructor() {}
+
+  changeSlide(index: number) : void {
+    this.currentSlideIndex = index;
+    clearInterval(this.slideInterval);
+    this.resetSlideInterval();
+  }
+
+  resetSlideInterval(): void {
+    this.slideInterval = setInterval(() : void => {
+    this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
+  }, 10000);
+}
+
+  ngOnInit() : void {
+    this.resetSlideInterval();
+  }
 }
