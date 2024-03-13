408f1ef6ae1bd4f4a4a1f3667772009a2b397c51
alexfilisanu <mihai.filisanu@gmail.com>
2024-03-13 23:08:31 +0200

    finish pages design

--- lung-cancer-detector-app/src/app/dashboard/dashboard.component.ts
+++ lung-cancer-detector-app/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,30 @@ import { Component } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
+export const Slides = [
+    'Scoala1 Scoala1 Scoala1 Scoala1 Scoala1 Scoala1',
+    'Scoala2 Scoala2 Scoala2 Scoala2 Scoala2 Scoala2',
+    'Scoala3 Scoala3 Scoala3 Scoala3 Scoala3 Scoala3'
+  ]
+
+export const Reviews = [
+    {
+      image: '../../assets/images/client.jpg',
+      name: 'Laviniu Serban',
+      comment: 'Multumesc ca mi ai salvat viata cu aplicatia ta minunata. Nu stiu ce as fi facut fara ea.'
+    },
+    {
+      image: '../../assets/images/client.jpg',
+      name: 'Caius',
+      comment: 'Aplicatia ta este minunata. O sa o recomand tuturor prietenilor mei.'
+    },
+    {
+      image: '../../assets/images/client.jpg',
+      name: 'Nicoleta Popescu',
+      comment: 'Cea mai buna aplicatie pe care am folosit-o vreodata. O sa o folosesc in continuare.'
+    }
+  ];
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -13,32 +37,55 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  public slides: Array<string> = ['Slide 1', 'Slide 2', 'Slide 3'];
+  public slides = Slides;
   public currentSlideIndex: number = 0;
+  public reviews = Reviews;
+  public currentReviewIndex: number = 0;
+
   private slideInterval: number | undefined;
 
   constructor() {}
 
   ngOnInit(): void {
     this.resetSlideInterval();
-    setTimeout(() => this.resetSlideInterval(), 100000);
   }
 
   ngOnDestroy(): void {
-    if (this.slideInterval) {
-      clearInterval(this.slideInterval);
-    }
+    this.clearSlideInterval();
   }
 
   public changeSlide(index: number): void {
     this.currentSlideIndex = index;
-    clearInterval(this.slideInterval);
     this.resetSlideInterval();
   }
 
-  public resetSlideInterval(): void {
-    this.slideInterval = setInterval((): void => {
+  private resetSlideInterval(): void {
+    this.clearSlideInterval();
+    this.slideInterval = setInterval(() => {
       this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
     }, 10000);
+  }
+
+  private clearSlideInterval(): void {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+      this.slideInterval = undefined;
+    }
+  }
+
+  public prev(): void {
+    if (this.currentReviewIndex > 0) {
+      this.currentReviewIndex--;
+    } else {
+      this.currentReviewIndex = this.reviews.length - 1;
+    }
+  }
+
+  public next(): void {
+    if (this.currentReviewIndex < this.reviews.length - 1) {
+      this.currentReviewIndex++;
+    } else {
+      this.currentReviewIndex = 0;
+    }
   }
 }
