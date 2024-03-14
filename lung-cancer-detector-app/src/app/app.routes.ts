2650a211e7f866da1302ccda7cb9a5ec2f08a564
alexfilisanu <mihai.filisanu@gmail.com>
2024-03-14 23:28:53 +0200

    added images

--- lung-cancer-detector-app/src/app/app.routes.ts
+++ lung-cancer-detector-app/src/app/app.routes.ts
@@ -4,8 +4,15 @@ import {DashboardComponent} from "./dashboard/dashboard.component";
 import {AdminLayoutComponent} from "./admin-layout/admin-layout.component";
 import {ContactUsComponent} from "./contact-us/contact-us.component";
 import {PredictionComponent} from "./prediction/prediction.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {AdvicesComponent} from "./advices/advices.component";
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
   {
     path: '',
     component: AdminLayoutComponent,
@@ -23,9 +30,17 @@ export const routes: Routes = [
         component: AboutComponent
       },
       {
+        path: 'advices',
+        component: AdvicesComponent
+      },
+      {
         path: 'contact-us',
         component: ContactUsComponent
       }
     ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
