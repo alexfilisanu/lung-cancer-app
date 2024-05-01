09382487d4782ae8d617b87c81435d52c2938ce5
alexfilisanu <mihai.filisanu@gmail.com>
2024-05-01 21:14:24 +0300

    register working with backend and db

--- lung-cancer-detector-app/src/app/register/register.component.ts
+++ lung-cancer-detector-app/src/app/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AbstractControl, FormBuilder, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from "@angular/forms";
 import { TranslateModule, TranslateService } from "@ngx-translate/core";
 import { NgClass } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -17,12 +19,32 @@ import { NgClass } from "@angular/common";
 })
 export class RegisterComponent {
 
-  public isVisiblePassword: Boolean = false;
-  public isVisibleConfirmPassword: Boolean = false;
+  public isVisiblePassword: boolean = false;
+  public isVisibleConfirmPassword: boolean = false;
+  public registerForm: any;
+  public errorMessage: string = '';
+  public isFormSubmitted: boolean = false;
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private formBuilder: FormBuilder, private http: HttpClient,
+              private router: Router) {
     const currentLanguage = sessionStorage.getItem('currentLanguage') || this.translate.getDefaultLang();
     this.translate.use(currentLanguage);
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6), this.matchPassword.bind(this)]]
+    });
+  }
+
+  private matchPassword(control: AbstractControl): ValidationErrors | null {
+    const password = control.parent?.get('password')?.value;
+    const confirmPassword = control.value;
+
+    return password === confirmPassword ? null : { mismatch: true };
   }
 
   public togglePassword(): void {
@@ -31,5 +53,41 @@ export class RegisterComponent {
 
   public toggleConfirmPassword(): void {
     this.isVisibleConfirmPassword = !this.isVisibleConfirmPassword;
+  }
+
+  onSubmit(): void {
+    this.isFormSubmitted = true;
+
+    if (this.registerForm?.valid) {
+      const formData = this.registerForm.value;
+      this.errorMessage = '';
+
+      this.http.post<any>('http://127.0.0.1:3100/auth/register', formData).subscribe({
+        next: () => {
+          this.router.navigate(['/dashboard']).catch(error => {
+            console.error('Navigation error:', error);
+          });
+        },
+        error: (error) => {
+          this.errorMessage = error.error.message;
+          console.error('Error occurred while registering the user:', error.error.message);
+        }
+      });
+    } else if (this.areAllFieldsCompleted() && this.registerForm?.get("email").invalid) {
+      this.errorMessage = 'error.invalid-email';
+    } else if (this.areAllFieldsCompleted() && this.registerForm?.get("password").invalid) {
+      this.errorMessage = 'error.invalid-password';
+    } else if (this.areAllFieldsCompleted() && this.registerForm?.get("confirmPassword").invalid) {
+      this.errorMessage = 'error.passwords-mismatch';
+    } else {
+      this.errorMessage = 'error.all-fields-required';
+    }
+  }
+
+  private areAllFieldsCompleted(): boolean {
+    return this.registerForm?.get("name").value
+      && this.registerForm?.get("email").value
+      && this.registerForm?.get("password").value
+      && this.registerForm?.get("confirmPassword").value;
   }
 }
